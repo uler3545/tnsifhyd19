bd6ee1a465fda4ae153d6a29e6d0f03a1f58d409
uler3545 <168516963+uler3545@users.noreply.github.com>
2024-05-10 15:15:51 +0530

    Add files via upload

---
+++ assignops.java
@@ -0,0 +1,25 @@
+package gillpack;
+
+public class assignops {
+	public static void main(String[] args) {
+		int x = 5;
+		x += 5;
+		System.out.println(x);
+		
+		int y = 5;
+		y -= 4;
+		System.out.println(y);
+		
+		int z = 6;
+		z*=3;
+		System.out.println(z);
+		
+		int i =8;
+		i/=2;
+		System.out.println(i);
+		
+		int j =8;
+		j%=2;
+		System.out.println(j);
+	}
+}
